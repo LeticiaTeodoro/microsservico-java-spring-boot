6be6dcc3d08b70ffe9f074824507b43c45d03739
Letícia Campos <leticia_isabelah@hotmail.com>
2024-02-17 12:38:55 -0300

    Balanceamento de carga com Ribbon

--- rh-worker/src/main/java/com/leticiacampos/rhworker/resources/WorkerResource.java
+++ rh-worker/src/main/java/com/leticiacampos/rhworker/resources/WorkerResource.java
@@ -2,7 +2,10 @@ package com.leticiacampos.rhworker.resources;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +18,11 @@ import com.leticiacampos.rhworker.repositories.WorkerRepository;
 @RestController
 @RequestMapping(value = "/workers")
 public class WorkerResource {
+	
+	private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
+	
+	@Autowired
+	private Environment environment;
 	
 	@Autowired
 	private WorkerRepository workerRepository;
@@ -29,6 +37,8 @@ public class WorkerResource {
 	
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Worker> findById(@PathVariable Long id){
+		
+		logger.info("Porta = " + environment.getProperty("local.server.port"));
 		
 		Worker worker = workerRepository.findById(id).get();
 		
