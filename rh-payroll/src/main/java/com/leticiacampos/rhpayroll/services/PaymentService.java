15edd42f2f60b50b2f5b2cef797bd463643e728e
Letícia Campos <leticia_isabelah@hotmail.com>
2024-02-16 22:53:04 -0300

    Implementação básica do RestTemplate

--- rh-payroll/src/main/java/com/leticiacampos/rhpayroll/services/PaymentService.java
+++ rh-payroll/src/main/java/com/leticiacampos/rhpayroll/services/PaymentService.java
@@ -1,13 +1,31 @@
 package com.leticiacampos.rhpayroll.services;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.leticiacampos.rhpayroll.entities.Payment;
+import com.leticiacampos.rhpayroll.entities.Worker;
 
 @Service
 public class PaymentService {
 	
+	@Value("${rh-worker.host}")
+	private String workerHost;
+	
+	@Autowired
+	private RestTemplate restTemplate;
+	
 	public Payment getPayment(long workerId, int days) {
-		return new Payment("Bob", 200.00, days);
+		
+		Map<String, String> uriVariables = new HashMap<>();
+		uriVariables.put("id", ""+workerId);
+		
+		Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 }
