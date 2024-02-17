2b9fd6a798291b5152ee251f912a252ea3876519
Letícia Campos <leticia_isabelah@hotmail.com>
2024-02-17 13:11:23 -0300

    Criando o projeto rh-eureka-server e configurando o servidor eureka

---
+++ rh-eureka-server/src/test/java/com/leticiacampos/rheurekaserver/RhEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leticiacampos.rheurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
