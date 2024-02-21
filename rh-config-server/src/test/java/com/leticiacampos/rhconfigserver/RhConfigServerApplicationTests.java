01531ae36fd42998c78ec7aa85f31f7a02d3ba47
Letícia Campos <leticia_isabelah@hotmail.com>
2024-02-21 20:29:36 -0300

    Criação do projeto rh-config-server

---
+++ rh-config-server/src/test/java/com/leticiacampos/rhconfigserver/RhConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leticiacampos.rhconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
